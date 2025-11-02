c4a45c735cfb4aa35db792e0899b504c44a2c3b1
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-11-02 11:54:47 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// /src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { analytics } from './lib/firebase' // <-- Add this import
 
-createRoot(document.getElementById("root")!).render(<App />);
+// We can log to the console when the promise resolves
+analytics.then(analyticsInstance => {
+  if (analyticsInstance) {
+    console.log("Firebase Analytics (GA4) has been initialized.");
+  } else {
+    console.warn("Firebase Analytics is not supported in this browser.");
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

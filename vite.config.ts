17179f3cd9ab675b40a0ddfc9445e10849bc6754
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-11-20 08:00:02 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 5000,
@@ -14,4 +14,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  esbuild: {
+    // Remove console.logs in production
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
+}));
